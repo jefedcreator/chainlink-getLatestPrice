5b55f45ca051d746c1ea0c4d86887ac0ab01691f
James Efienemokwu <jefiene77@gmail.com>
2022-03-15 19:18:44 +0100

    swapper functionality

--- contracts/chainlink.sol
+++ contracts/chainlink.sol
@@ -3,9 +3,33 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract PriceConsumerV3 {
+contract PriceConsumerV3{
 
     AggregatorV3Interface internal priceFeed;
+
+    struct swapInfo{
+        address swapFrom;
+        address swapTo;
+        address ownerAddress;
+        uint usdcbalance;
+        uint ethbalance;
+    }
+
+    struct buyerInfo{
+        address ownedtoken;
+        uint buyAmount;
+        uint balance;
+        uint usdcbalance;
+        uint ethbalance;
+    }
+
+    uint decimal = 10**18;
+
+    uint swapId;
+
+    mapping(uint => swapInfo) offer;
+
+    mapping(address => buyerInfo) buyer;
 
     /**
      * Network: Kovan
@@ -28,5 +52,35 @@ contract PriceConsumerV3 {
             /*uint80 answeredInRound*/
         ) = priceFeed.latestRoundData();
         return price;
+    }
+
+    function swapTokens(address _eth, uint _amount, address _usdc, address _owner) public{
+        swapInfo storage s = offer[swapId];
+        s.swapFrom = _eth;
+        s.swapTo = _usdc;
+        s.ownerAddress = _owner;
+        s.usdcbalance = 0;
+        s.ethbalance = _amount;
+        swapId++;
+    }
+
+    function swapper(uint _swapId, uint _amount, address _buyerAdd, address _usdc) public{
+        swapInfo storage sI = offer[_swapId];
+        // require(sI.amount, "amount too low");
+        uint rate = uint(getLatestPrice());
+        uint sendUsdcAmount = rate / _amount;
+        uint ethAmount = rate * _amount;
+        // IERC20(sI.swapFrom).transfer(sI.ownerAddress, sendAmount);
+        // IERC20(sI.swapTo).transferFrom(sI.ownerAddress,msg.sender , _amount);
+        buyer[_buyerAdd].ownedtoken = _usdc;
+        buyer[_buyerAdd].buyAmount = _amount;
+        sI.usdcbalance += sendUsdcAmount;
+        buyer[_buyerAdd].usdcbalance -= sendUsdcAmount;
+        sI.ethbalance -= ethAmount;
+        buyer[_buyerAdd].ethbalance += ethAmount;
+    }
+
+    function buyerBal(address _buyerAdd) public view returns(uint){
+        return buyer[_buyerAdd].ethbalance;
     }
 }
--- scripts/deployChainlink.ts
+++ scripts/deployChainlink.ts
@@ -14,15 +14,28 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const BTC_ETH = "0x2431452A0010a43878bF198e170F6319Af6d27F4"
-
-
+  const USDC_ETH = "0xdCA36F27cbC4E38aE16C4E9f99D39b42337F6dcf"
+  const RINKEBY_USDC = "0xeb8f08a975Ab53E34D8a0330E0D34de942C95926";
+  const RINKEBY_ETH = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709";
+  const RANDADDRESS = ""
+  const RANDADDRESS2 = ""
 
   const chainlink = await ethers.getContractFactory("PriceConsumerV3");
-  const getChainlink = await chainlink.deploy(BTC_ETH);
+  const getChainlink = await chainlink.deploy(USDC_ETH);
 
   const showdetails = await getChainlink.deployed();
-  console.log(await showdetails.getLatestPrice());
+  const getLatestPrice = await showdetails.getLatestPrice();
+  const swapTokens = await showdetails.swapTokens(RINKEBY_ETH, 100,RINKEBY_USDC,RANDADDRESS);
+  const swapper = await showdetails.swapper(0,20,RANDADDRESS2,RINKEBY_USDC);
+  const buyerBal = await showdetails.buyerBal(RANDADDRESS2);
+
+  console.log(getLatestPrice);
+  console.log(swapTokens);
+  console.log(swapper);
+  console.log(buyerBal);
+  
+  
+  
 
 
   console.log("Greeter deployed to:", getChainlink.address);
